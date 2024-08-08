3a5471136410a29b6eb8478206916f2defba45f8
DinelyWellehewage <dinelywh78@gmail.com>
2024-08-08 12:32:25 +0200

    setup authentication with JPA

--- src/main/java/com/example/spring_security_jpa/SpringSecurityJpaApplication.java
+++ src/main/java/com/example/spring_security_jpa/SpringSecurityJpaApplication.java
@@ -1,12 +1,13 @@
 package com.example.spring_security_jpa;
 
+import com.example.spring_security_jpa.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
 public class SpringSecurityJpaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityJpaApplication.class, args);
 	}
--- src/main/java/com/example/spring_security_jpa/config/MyUserDetailsService.java
+++ src/main/java/com/example/spring_security_jpa/config/MyUserDetailsService.java
@@ -1,15 +1,24 @@
 package com.example.spring_security_jpa.config;
 
+import com.example.spring_security_jpa.models.User;
+import com.example.spring_security_jpa.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class MyUserDetailsService implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return new MyUserDetails(username);
+        Optional<User> user = userRepository.findByUserName(username);
+        user.orElseThrow(()->new UsernameNotFoundException("Not Found: "+ username));
+        return user.map(MyUserDetailsJPA::new).get();
     }
 }
